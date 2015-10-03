f3852be5dee3b1be35352cc34b5df359c7bba0cb
krngrvr09 <krngrvr09@gmail.com>
2015-10-03 21:52:13 +0530

    updated workflow

--- pomodoro.py
+++ pomodoro.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import time
+import datetime
 if len(sys.argv) > 1:
 	type = sys.argv[1]
 
@@ -8,18 +9,27 @@ if type == "work":
 	os.system("notify-send 'Starting 25 minutes timer.'")
 	os.system("cat original.txt > /etc/hosts")
 	os.system("cat block.txt >> /etc/hosts")
+	starttime = str(datetime.datetime.now())
 	for i in range(25):
 		print 25-i," minutes to go..."
 		time.sleep(60)
+	endtime = str(datetime.datetime.now())
 	os.system("notify-send 'Work time ends.'")
+	work = raw_input("What did you do?")
+	os.system("echo 'WORK - From %s to %s - %s' >> work_done.txt"%(starttime, endtime, work))
+	
 	
 elif(type == "break"):
 	os.system("notify-send 'Starting 5 minutes break.'")
 	os.system("cat original.txt > /etc/hosts")
+	starttime = str(datetime.datetime.now())
 	for i in range(5):
 		print 5-i," minutes to go..."
-		time.sleep(1)
+		time.sleep(60)
+	endtime = str(datetime.datetime.now())
 	os.system("notify-send 'Break time ends.'")
+	work = raw_input("What did you do?")
+	os.system("echo 'REST - From %s to %s - %s' >> work_done.txt"%(starttime, endtime, work))
 		
 	
 else:
