f6fe35b770385ecd5e4e6b37ee3a7d1b018970cc
krngrvr09 <krngrvr09@gmail.com>
2015-10-04 22:45:15 +0530

    minor changes

--- pomodoro.py
+++ pomodoro.py
@@ -16,21 +16,26 @@ if type == "work":
 	endtime = str(datetime.datetime.now())
 	os.system("notify-send 'Work time ends.'")
 	work = raw_input("What did you do?")
-	os.system("echo 'WORK - From %s to %s - %s' >> work_done.txt"%(starttime, endtime, work))
+	os.system("echo 'WORK - From %s to %s - %s' >> 4_october_2015.txt"%(starttime, endtime, work))
 	
 	
 elif(type == "break"):
-	os.system("notify-send 'Starting 5 minutes break.'")
+	break_duration = int(raw_input("Break Duration: "))
+	# notification = "Starting break for %d minutes"
+	os.system("notify-send 'Starting %d minutes break.'"%break_duration)
 	os.system("cat original.txt > /etc/hosts")
 	starttime = str(datetime.datetime.now())
-	for i in range(5):
-		print 5-i," minutes to go..."
+	for i in range(break_duration):
+		print break_duration-i," minutes to go..."
 		time.sleep(60)
 	endtime = str(datetime.datetime.now())
 	os.system("notify-send 'Break time ends.'")
 	work = raw_input("What did you do?")
-	os.system("echo 'REST - From %s to %s - %s' >> work_done.txt"%(starttime, endtime, work))
+	os.system("echo 'REST - From %s to %s - %s' >> 4_october_2015.txt"%(starttime, endtime, work))
 		
+elif type == "sleep":
+	os.system("notify-send 'Preparing for sleep'")
+	os.system("echo 'SLEEP' >> 4_october_2015.txt")
 	
 else:
 	print "else"
